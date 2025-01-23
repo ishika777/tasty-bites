392b956afadcc8dc002cc8b70a5a1ab44ea5922e
ishika777 <145911895+ishika777@users.noreply.github.com>
2025-01-23 21:32:48 +0530

    Update order.route.ts

--- server/routes/order.route.ts
+++ server/routes/order.route.ts
@@ -5,7 +5,7 @@ import upload from "../middlwares/multer";
 const router = express.Router()
 
 router.route("/").post(isAuthenticated, getOrders)
-router.route("/checkout/create-checkout-session").post(isAuthenticated, createCheckoutSession)
+// router.route("/checkout/create-checkout-session").post(isAuthenticated, createCheckoutSession)
 // router.route("/webhook").post()
 
 export default router;

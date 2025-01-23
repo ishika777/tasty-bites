0b1cc59167422bb0a6ae6e6caef5cf838f66f5ff
ishika777 <145911895+ishika777@users.noreply.github.com>
2025-01-23 21:46:00 +0530

    Update constants.ts

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT = "http://localhost:3000/api/v1/user"
-export const RESTAURANT_API_END_POINT = "http://localhost:3000/api/v1/restaurant"
-export const MENU_API_END_POINT = "http://localhost:3000/api/v1/menu"
-export const ORDER_API_END_POINT = "http://localhost:3000/api/v1/order"
+export const USER_API_END_POINT = "https://tasty-bites-9f9m.onrender.com/api/v1/user"
+export const RESTAURANT_API_END_POINT = "https://tasty-bites-9f9m.onrender.com/api/v1/restaurant"
+export const MENU_API_END_POINT = "https://tasty-bites-9f9m.onrender.com/api/v1/menu"
+export const ORDER_API_END_POINT = "https://tasty-bites-9f9m.onrender.com/api/v1/order"

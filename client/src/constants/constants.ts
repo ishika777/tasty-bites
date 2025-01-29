b59587ede867ba0123fb63f8768db9ebfcb4da7f
Ishika Knsal <ishikakasal05@gmail.com>
2025-01-29 08:39:34 +0530

    update constants.ts

--- client/src/constants/constants.ts
+++ client/src/constants/constants.ts
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:5173/api/v1/user"
-export const RESTAURANT_API_END_POINT = "http://localhost:5173/api/v1/restaurant"
-export const MENU_API_END_POINT = "http://localhost:5173/api/v1/menu"
-export const ORDER_API_END_POINT = "http://localhost:5173/api/v1/order"
+export const USER_API_END_POINT = "http://localhost:3000/api/v1/user"
+export const RESTAURANT_API_END_POINT = "http://localhost:3000/api/v1/restaurant"
+export const MENU_API_END_POINT = "http://localhost:3000/api/v1/menu"
+export const ORDER_API_END_POINT = "http://localhost:3000/api/v1/order"
 
